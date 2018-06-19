39732f3cefda3f0fc0f6ec83c311295ab1130ac3
quyvn96 <quyvn317@gmail.com>
2018-06-19 13:21:14 +0700

    Lesson 10: building structure for app

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
+import { PersonComponent } from './person/person.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [MainComponent]
+  declarations: [MainComponent, PersonComponent]
 })
 export class MainModule { }
