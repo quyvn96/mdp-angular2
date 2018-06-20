504785c687dc6a2d13c4230b970a854e9ea9f7d2
quyvn96 <quyvn317@gmail.com>
2018-06-21 01:13:58 +0700

    lesson13 add UI admin for app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,AfterViewChecked,ElementRef } from '@angular/core';
+import { elementDef } from '@angular/core/src/view';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'welcome to angular 2!';
+export class AppComponent implements AfterViewChecked{
+  constructor(private elementDef:ElementRef){
+
+  }
+  ngAfterViewChecked(){
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.src= "../assets/js/custom.js";
+    this.elementDef.nativeElement.appendChild(s);
+  }
 }
