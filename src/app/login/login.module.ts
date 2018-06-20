f6a997ba120284ce05338ff6ea4396533c12e445
quyvn96 <quyvn317@gmail.com>
2018-06-20 22:20:55 +0700

    lesson11:bulid router for app

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import{Routes,RouterModule} from '@angular/router';
 import { LoginComponent } from './login.component';
-
+export const routers:Routes = [
+    {path:'',component:LoginComponent}
+];
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routers)
   ],
   declarations: [LoginComponent]
 })
