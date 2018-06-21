3e2f6523bf1106ffa14b16a107f8ceacba824570
quyvn96 <quyvn317@gmail.com>
2018-06-21 15:02:40 +0700

    lesson16: Login and logout

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import{Routes,RouterModule} from '@angular/router';
 import { LoginComponent } from './login.component';
+import {FormsModule} from '@angular/forms';
+import { NotificationService } from '../core/services/notification.service';
+import { AuthenService } from '../core/services/authen.service';
 export const routers:Routes = [
     {path:'',component:LoginComponent}
 ];
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(routers)
   ],
+  providers:[AuthenService,NotificationService],  
   declarations: [LoginComponent]
 })
 export class LoginModule { }
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -5,12 +5,15 @@ import { PersonComponent } from './person/person.component';
 import { mainRoutes } from './main.routes';
 import { RouterModule } from '@angular/router';
 import { UserModule } from './user/user.module';
+import { UtilityService } from '../core/services/utility.service';
+import { AuthenService } from '../core/services/authen.service';
 @NgModule({
   imports: [
     CommonModule,
     UserModule,
     RouterModule.forChild(mainRoutes)
   ],
+  providers: [UtilityService,AuthenService],
   declarations: [MainComponent, PersonComponent]
 })
 export class MainModule { }
