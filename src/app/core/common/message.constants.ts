1281105f5b3680e28fea9a0fbe2feceb28d5f84e
quyvn96 <quyvn317@gmail.com>
2018-06-21 10:14:03 +0700

    lesson14:build common service - part 1

---
+++ src/app/core/common/message.constants.ts
@@ -0,0 +1,8 @@
+export class MessageConstants {
+    public static SYSTEM_ERROR_MSG = "Có lỗi kết nối tới server";
+    public static CONFIRM_DELETE_MSG = "Bạn có chắc chắn muốn xóa bản ghi này?";
+    public static LOGIN_AGAIN_MSG = "Hết phiên đăng nhập. Yêu cầu đăng nhập lại";
+    public static CREATED_OK_MSG = "Thêm mới thành công";
+    public static UPDATED_OK_MSG = "Cập nhật thành công";
+    public static DELETED_OK_MSG = "Xóa thành công";
+}
