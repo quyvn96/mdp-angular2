cad308dea0b39ac4270d30cf7ce3a45b318bcef7
quyvn96 <quyvn317@gmail.com>
2018-06-22 14:11:55 +0700

    lesson18: building role group

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -5,37 +5,57 @@ import { SystemConstants } from './../common/system.constants';
 import { AuthenService } from './authen.service';
 import { NotificationService } from './notification.service';
 import { UtilityService } from './utility.service';
+
 import { Observable } from 'rxjs/Observable';
 import { MessageConstants } from './../common/message.constants';
+
 @Injectable()
 export class DataService {
   private headers: Headers;
-  constructor(private _http: Http, private _router: Router, private _authenService: AuthenService
-    , private _notificationService: NotificationService, private _utilityService: UtilityService) { }
+  constructor(private _http: Http, private _router: Router, private _authenService: AuthenService,
+    private _notificationService: NotificationService, private _utilityService: UtilityService) {
+    this.headers = new Headers();
+    this.headers.append('Content-Type', 'application/json');
+  }
+
   get(uri: string) {
     this.headers.delete("Authorization");
-    this.headers.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().access_token);
+    this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
     return this._http.get(SystemConstants.BASE_API + uri, { headers: this.headers }).map(this.extractData);
   }
   post(uri: string, data?: any) {
     this.headers.delete("Authorization");
-    this.headers.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().access_token);
+    this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
     return this._http.post(SystemConstants.BASE_API + uri, data, { headers: this.headers }).map(this.extractData);
   }
   put(uri: string, data?: any) {
     this.headers.delete("Authorization");
-    this.headers.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().access_token);
+    this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
     return this._http.put(SystemConstants.BASE_API + uri, data, { headers: this.headers }).map(this.extractData);
   }
   delete(uri: string, key: string, id: string) {
     this.headers.delete("Authorization");
-    this.headers.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().access_token);
-    return this._http.put(SystemConstants.BASE_API + uri + "/?" + key + "=" + id, { headers: this.headers }).map(this.extractData);
+    this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
+    return this._http.delete(SystemConstants.BASE_API + uri + "/?" + key + "=" + id, { headers: this.headers })
+      .map(this.extractData);
+  }
+  deleteWithMultiParams(uri: string, params) {
+    this.headers.delete('Authorization');
+
+    this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
+    var paramStr: string = '';
+    for (let param in params) {
+      paramStr += param + "=" + params[param] + '&';
+    }
+    return this._http.delete(SystemConstants.BASE_API + uri + "/?" + paramStr, { headers: this.headers })
+      .map(this.extractData);
+
   }
   postFile(uri: string, data?: any) {
     let newHeader = new Headers();
-    newHeader.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().access_token);
-    return this._http.post(SystemConstants.BASE_API + uri, data, { headers: newHeader }).map(this.extractData);
+    newHeader.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
+    return this._http.post(SystemConstants.BASE_API + uri, data, { headers: newHeader })
+      .map(this.extractData);
   }
   private extractData(res: Response) {
     let body = res.json();
@@ -58,6 +78,7 @@ export class DataService {
 
       return Observable.throw(errMsg);
     }
-  }
 
+
+  }
 }
