3a6f6ee504cb6a9207b5ac723c2d4e93218a4689
quyvn96 <quyvn317@gmail.com>
2018-09-29 12:39:34 +0700

    commit test

--- src/app/main/function/function.component.ts
+++ src/app/main/function/function.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TreeComponent } from 'angular-tree-component';
+import { DataService } from '../../core/services/data.service';
+import { NotificationService } from '../../core/services/notification.service';
+import { MessageConstants } from '../../core/common/message.constants';
+import { UtilityService } from '../../core/services/utility.service';
 @Component({
   selector: 'app-function',
   templateUrl: './function.component.html',
   styleUrls: ['./function.component.css']
 })
 export class FunctionComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild(TreeComponent)
+  private treeFunction: TreeComponent;
+  public _functionHierachy: any[];
+  public _functions: any;
+  public entity: any;
+  public editFlag: boolean;
+  public filter: string = '';
+  constructor(private dataService: DataService,
+    private notificationService: NotificationService,
+    private utilityService: UtilityService) { }
 
   ngOnInit() {
+    this.search();
   }
-
+  public search() {
+    this.dataService.get('/api/function/getall?filter=' + this.filter)
+      .subscribe((response: any[]) => {
+        this._functions = response.filter(x => x.ParentId == null);
+        this._functionHierachy = this.utilityService.Unflatten(response);
+      }, error => this.dataService.handleError(error));
+  }
 }
--- src/app/main/function/function.module.ts
+++ src/app/main/function/function.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FunctionComponent } from './function.component';
-
+import { Routes, RouterModule } from '@angular/router';
+import { TreeModule } from 'angular-tree-component';
+import { FormsModule } from '@angular/forms'
+const functionRoutes: Routes = [
+  { path: '', redirectTo: 'index', pathMatch: 'full' },
+  { path: 'index', component: FunctionComponent }
+]
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(functionRoutes),
+    TreeModule,
+    FormsModule
   ],
   declarations: [FunctionComponent]
 })
